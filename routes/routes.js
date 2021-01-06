1dccae5831037d158c3f2134cd881e05b0babe29
soham874 <soham874@gmail.com>
2021-01-07 02:34:48 +0530

    Added : regex verification

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ let controller = require('../Controller/controller')
 
 route.post('/EmployeeCreate', controller.createController)
 route.get('/EmployeeRead', controller.getController)
-route.delete('/EmployeeDelete', controller.deleteController)
 route.patch('/EmployeeUpdate', controller.updateController)
+route.delete('/EmployeeDelete', controller.deleteController)
 
 module.exports = route;
