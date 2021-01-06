8097b0465bbf724852b0c0399687b893168df213
soham874 <soham874@gmail.com>
2021-01-06 14:34:18 +0530

    Added : Controller scripts

--- Controller/controller.js
+++ Controller/controller.js
@@ -1,7 +1,27 @@
 let response = {}
 let services = require('../Services/services')
+let serverCode
 
 class controller {
 
+    addController = (req, res) => {
+        services.addService(req.body).then(
+            (result) => {
+                response.success = true
+                response.message = result.message
+                response.data = result.data
+                serverCode = 200
+            },
+            (error) => {
+                response.success = false
+                response.message = error.message
+                response.data = error.data
+                serverCode = 400
+            }
+        )
+        return res.status(serverCode).send(response)
+    }
 
 }
+
+module.exports = new controller()
