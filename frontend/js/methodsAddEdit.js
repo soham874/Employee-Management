ce130e4b63cc4548a8c407dd072574d8a93c09ff
soham874 <soham874@gmail.com>
2021-01-11 19:52:03 +0530

    Refactored : Modified code to prevent unnecessary input

--- frontend/js/methodsAddEdit.js
+++ frontend/js/methodsAddEdit.js
@@ -11,7 +11,8 @@ populate = () => {
     data = JSON.parse(sessionStorage.getItem('data'))
 
     if (id != 0)
-        for (let i = 0; i < 7; i++) {
-            document.getElementById(`field${i+1}`).value = data[id][keys[i]]
-        }
+        for (let i = 0; i < 7; i++)
+            if (data[id - 1][keys[i]] != null)
+                document.getElementById(`field${i+1}`).value = data[id - 1][keys[i]]
+
 }
