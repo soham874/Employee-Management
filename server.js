ae145f48aafc05c2de417297767bb9cc88d3c736
soham874 <soham874@gmail.com>
2021-01-14 10:51:44 +0530

    Added : CORS middleware support

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const route = require('./routes/routes')
 
 let port = 3000;
 
+app.use(cors())
 app.use(express.json())
 app.use('/', route)
 
